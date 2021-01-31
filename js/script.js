9ef18d4e41a2d11494ca14bd2db66c672debaf2d
Junior <juon477@hotmail.com>
2021-01-31 00:51:18 -0500

    primera parte

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//query Selector 
+
+const heading = document.querySelector('#heading');
+
+heading.textContent = 'Nuevo Heading';
+
+// Generar Nuevo Enlace
+
+const idioma = document.createElement('A')
+
+// Agregar href 
+idioma.href = 'nuevo-enlace.html';
+
+// Agregar el texto 
+idioma.textContent = 'Idiomas';
+
+// Agregar la clase 
+idioma.classList.add('navegacion_enlace');
+
+// Agregar al Documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(idioma);
+
+console.log(idioma);
+
+window.onscroll = function(){
+    console.log('jodete...');
+}
